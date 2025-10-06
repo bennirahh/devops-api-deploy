1a41e21f415c15020d94f4406b9f55fc3d807126
bennirahh <79867738+bennirahh@users.noreply.github.com>
2025-10-06 15:17:16 +0200

    fix port host

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import product from "./routes/product.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || '0.0.0.0';
-
+const HOST = process.env.HOST || "127.0.0.1"; 
 app.get("/", (req, res) => {
   res.json({ message: "Bienvenue sur notre API en Node.js (version moderne)" });
 });
